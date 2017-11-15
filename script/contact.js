abdcfd451a1a8ed4804c1b9b4fd79778a0d9a1fd
Michał Parysz <michalparysz@MacBook-Air-Micha.local>
2017-11-15 17:26:06 +0100

    refactor of contact.js

--- script/contact.js
+++ script/contact.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
             var inputThree = $('form').children('.contactInputThree');
             var inputFour = $('form').children('.contactInputFour');
             var TEXTBOX_MAX_CHARACTERS = 2000;
-            var inputValidationOne = 0;
-            var inputValidationTwo = 0;
-            var inputValidationThree = 0;
-            var inputValidationFour = 0;
+            var INPUTBOX_MAX_CHARACTERS = 40;
+            var inputValidationOne = false;
+            var inputValidationTwo = false;
+            var inputValidationThree = false;
+            var inputValidationFour = false;
             var isDiscovered = false;
             $('.mapToggle').click(function(){
-                if(!isDiscovered){
+                if(!isDiscovered) {
                     $(this).addClass('buttonClick');
                     $('.map').animate({
                         height: "450px",
@@ -19,7 +20,7 @@ $(document).ready(function() {
                     $('html,body').animate({
                         scrollTop: $('.map').offset().top}, 'slow');
                     isDiscovered = true;
-                }else{
+                } else {
                     $(this).removeClass('buttonClick');
                     $('.map').animate({
                         height: "0px",
@@ -33,80 +34,80 @@ $(document).ready(function() {
                     inputTwo.val('');
                     inputThree.val('');
                     inputFour.val('');
-                    inputValidationOne = 0;
-                    inputValidationTwo = 0;
-                    inputValidationThree = 0;
-                    inputValidationFour = 0;
-                    $('form').children('.contactCounterOne').text(40);
-                    $('form').children('.contactCounterTwo').text(40);
-                    $('form').children('.contactCounterThree').text(40);
+                    inputValidationOne = false;
+                    inputValidationTwo = false;
+                    inputValidationThree = false;
+                    inputValidationFour = false;
+                    $('form').children('.contactCounterOne').text(INPUTBOX_MAX_CHARACTERS);
+                    $('form').children('.contactCounterTwo').text(INPUTBOX_MAX_CHARACTERS);
+                    $('form').children('.contactCounterThree').text(INPUTBOX_MAX_CHARACTERS);
                     $('form').children('.contactCounterFour').text(TEXTBOX_MAX_CHARACTERS);
                     $('form').children('.contactInputFive').attr("disabled", "disabled");
                 });
-                inputOne.keyup(function(){
-                    if($(this).val() != ''){
-                        inputValidationOne = 1;
+                inputOne.keyup(function () {
+                    if($(this).val() != '') {
+                        inputValidationOne = true;
                     }else{
-                        inputValidationOne = 0;
+                        inputValidationOne = false;
                     }
-                    var inputStringCounter = 40 - $(this).val().length;
+                    var inputStringCounter = INPUTBOX_MAX_CHARACTERS - $(this).val().length;
                     $('form').children('.contactCounterOne').text(inputStringCounter);
 
-                    if($(this).val().length > 40)
+                    if($(this).val().length > INPUTBOX_MAX_CHARACTERS)
                     {
-                        inputValidationOne = 0;
+                        inputValidationOne = false;
                         $('form').children('.contactInputFive').attr("disabled", "disabled");
                     }
 
                 });
-                inputTwo.keyup(function(){
-                    if($(this).val() != ''){
-                        inputValidationTwo = 1;
-                    }else{
-                        inputValidationTwo = 0;
+                inputTwo.keyup(function () {
+                    if($(this).val() != '') {
+                        inputValidationTwo = true;
+                    } else {
+                        inputValidationTwo = false;
                     }
-                    var inputStringCounter = 40 - $(this).val().length;
+                    var inputStringCounter = INPUTBOX_MAX_CHARACTERS - $(this).val().length;
                     $('form').children('.contactCounterTwo').text(inputStringCounter);
-                    if($(this).val().length > 40)
+                    if($(this).val().length > INPUTBOX_MAX_CHARACTERS)
                     {
-                        inputValidationTwo = 0;
+                        inputValidationTwo = false;
                         $('form').children('.contactInputFive').attr("disabled", "disabled");
                     }
 
                 });
-                inputThree.keyup(function(){
-                    if($(this).val() != ''){
-                        inputValidationThree = 1;
+                inputThree.keyup(function () {
+                    if($(this).val() != '') {
+                        inputValidationThree = true;
                     }else{
-                        inputValidationThree = 0;
+                        inputValidationThree = false;
                     }
-                    var inputStringCounter = 40 - $(this).val().length;
+                    var inputStringCounter = INPUTBOX_MAX_CHARACTERS - $(this).val().length;
                     $('form').children('.contactCounterThree').text(inputStringCounter);
-                    if($(this).val().length > 40)
+                    if($(this).val().length > INPUTBOX_MAX_CHARACTERS)
                     {
-                        inputValidationThree = 0;
+                        inputValidationThree = false;
                         $('form').children('.contactInputFive').attr("disabled", "disabled");
                     }
 
                 });
-                inputFour.keyup(function(){
-                    if($(this).val() != ''){
-                        inputValidationFour = 1;
+                inputFour.keyup(function () {
+                    if($(this).val() != '') {
+                        inputValidationFour = true;
                     }else{
-                        inputValidationFour = 0;
+                        inputValidationFour = false;
                     }
                     var inputStringCounter = TEXTBOX_MAX_CHARACTERS - $(this).val().length;
                     $('form').children('.contactCounterFour').text(inputStringCounter);
                     if($(this).val().length > TEXTBOX_MAX_CHARACTERS)
                     {
-                        inputValidationFour = 0;
+                        inputValidationFour = false;
                         $('form').children('.contactInputFive').attr("disabled", "disabled");
                     }
 
                 });
 
-                $('form').keyup(function(){
-                    if(inputValidationOne == 1 && inputValidationTwo == 1 && inputValidationThree == 1 && inputValidationFour == 1){
+                $('form').keyup(function () {
+                    if(inputValidationOne == 1 && inputValidationTwo == 1 && inputValidationThree == 1 && inputValidationFour == 1) {
                         $('form').children('.contactInputFive').removeAttr("disabled");
                     }else{
                         $('form').children('.contactInputFive').attr("disabled", "disabled");
